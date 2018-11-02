61ba3b28702ddeea6d136601e43c606e8631f43d
LeHaine <colt.daily@gmail.com>
2018-11-02 19:38:44 -0400

    fix manifest 404 error. add spread operator babel plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -8,71 +8,72 @@ var phaserModule = path.join(__dirname, "/node_modules/phaser/");
 var phaser = path.join(phaserModule, "src/phaser.js");
 
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "true"))
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "true"))
 });
 
 module.exports = {
-  entry: {
-    app: ["babel-polyfill", path.resolve(__dirname, "src/main.js")],
-    vendor: ["phaser", "webfontloader"]
-  },
-  devtool: "cheap-source-map",
-  output: {
-    pathinfo: true,
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "./dist/",
-    filename: "bundle.js"
-  },
-  watch: true,
-  plugins: [
-    definePlugin,
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor" /* chunkName= */,
-      filename: "vendor.bundle.js" /* filename= */
-    }),
-    new HtmlWebpackPlugin({
-      filename: "../index.html",
-      template: "./public/index.html",
-      favicon: "./public/favicon.ico",
-      chunks: ["vendor", "app"],
-      chunksSortMode: "manual",
-      hash: false
-    }),
-    new BrowserSyncPlugin({
-      host: process.env.IP || "localhost",
-      port: process.env.PORT || 3000,
-      server: {
-        baseDir: ["./", "./build"]
-      },
-      target: "http://localhost:8080",
-      ws: true
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve("file-loader"),
-        options: {
-          name: "/assets/[name].[hash:8].[ext]"
+    entry: {
+        app: ["babel-polyfill", path.resolve(__dirname, "src/main.js")],
+        vendor: ["phaser", "webfontloader"]
+    },
+    devtool: "cheap-source-map",
+    output: {
+        pathinfo: true,
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "./dist/",
+        filename: "bundle.js"
+    },
+    watch: true,
+    plugins: [
+        definePlugin,
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor" /* chunkName= */,
+            filename: "vendor.bundle.js" /* filename= */
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./public/index.html",
+            favicon: "./public/favicon.ico",
+            manifest: "./public/manifest.json",
+            chunks: ["vendor", "app"],
+            chunksSortMode: "manual",
+            hash: false
+        }),
+        new BrowserSyncPlugin({
+            host: process.env.IP || "localhost",
+            port: process.env.PORT || 3000,
+            server: {
+                baseDir: ["./", "./dist"]
+            },
+            target: "http://localhost:8080",
+            ws: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve("file-loader"),
+                options: {
+                    name: "/assets/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                include: path.join(__dirname, "src")
+            },
+            { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] }
+        ]
+    },
+    node: {
+        fs: "empty",
+        net: "empty",
+        tls: "empty"
+    },
+    resolve: {
+        alias: {
+            phaser: phaser
         }
-      },
-      {
-        test: /\.js$/,
-        use: ["babel-loader"],
-        include: path.join(__dirname, "src")
-      },
-      { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] }
-    ]
-  },
-  node: {
-    fs: "empty",
-    net: "empty",
-    tls: "empty"
-  },
-  resolve: {
-    alias: {
-      phaser: phaser
     }
-  }
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,76 +10,74 @@ var phaserModule = path.join(__dirname, "/node_modules/phaser/");
 var phaser = path.join(phaserModule, "src/phaser.js");
 
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "false"))
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "false"))
 });
 
 module.exports = {
-  entry: {
-    app: ["babel-polyfill", path.resolve(__dirname, "src/main.js")],
-    vendor: ["phaser", "webfontloader"]
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "./",
-    filename: "js/bundle.js"
-  },
-  plugins: [
-    definePlugin,
-    new CleanWebpackPlugin(["build"]),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
-      drop_console: true,
-      minimize: true,
-      output: {
-        comments: false
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor" /* chunkName= */,
-      filename: "js/vendor.bundle.js" /* filename= */
-    }),
-    new HtmlWebpackPlugin({
-      filename: "index.html", // path.resolve(__dirname, 'build', 'index.html'),
-      template: "./public/index.html",
-      favicon: "./public/favicon.ico",
-      chunks: ["vendor", "app"],
-      chunksSortMode: "manual",
-      hash: true
-    }),
-    new CopyWebpackPlugin([
-      { from: "assets", to: "assets" },
-      { from: "src/manifest.json", to: "./manifest.json" }
-    ]),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve("file-loader"),
-        options: {
-          name: "/assets/[name].[hash:8].[ext]"
+    entry: {
+        app: ["babel-polyfill", path.resolve(__dirname, "src/main.js")],
+        vendor: ["phaser", "webfontloader"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "./",
+        filename: "js/bundle.js"
+    },
+    plugins: [
+        definePlugin,
+        new CleanWebpackPlugin(["build"]),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.optimize.UglifyJsPlugin({
+            drop_console: true,
+            minimize: true,
+            output: {
+                comments: false
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor" /* chunkName= */,
+            filename: "js/vendor.bundle.js" /* filename= */
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html", // path.resolve(__dirname, 'build', 'index.html'),
+            template: "./public/index.html",
+            favicon: "./public/favicon.ico",
+            manifest: "./public/manifest.json",
+            chunks: ["vendor", "app"],
+            chunksSortMode: "manual",
+            hash: true
+        }),
+        new CopyWebpackPlugin([{ from: "assets", to: "assets" }]),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve("file-loader"),
+                options: {
+                    name: "/assets/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                include: path.join(__dirname, "src")
+            },
+            { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] }
+        ]
+    },
+    node: {
+        fs: "empty",
+        net: "empty",
+        tls: "empty"
+    },
+    resolve: {
+        alias: {
+            phaser: phaser
         }
-      },
-      {
-        test: /\.js$/,
-        use: ["babel-loader"],
-        include: path.join(__dirname, "src")
-      },
-      { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] }
-    ]
-  },
-  node: {
-    fs: "empty",
-    net: "empty",
-    tls: "empty"
-  },
-  resolve: {
-    alias: {
-      phaser: phaser
     }
-  }
 };
